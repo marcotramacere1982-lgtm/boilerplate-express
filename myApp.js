524d82dbc9bab18fdd051d1af78cba2f17dec1ca
marcotramacere1982-lgtm <marcotramacere1982@gmail.com>
2026-04-15 13:26:46 +0000

    abspush

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
 //  res.send("Hello Express");
 const absolutePath = __dirname + "/views/index.html";

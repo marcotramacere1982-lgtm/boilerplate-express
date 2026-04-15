df8d8b86d97fb77b68ca7a66e7ccce72078fdd88
marcotramacere1982-lgtm <marcotramacere1982@gmail.com>
2026-04-15 14:19:34 +0000

    time

--- myApp.js
+++ myApp.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 //console.log("Hello World");
+app.use(function(req, res, next) {
+
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+
+  console.log(`${method} ${path} - ${ip}`);
+   next();
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
 //  res.send("Hello Express");
@@ -9,9 +20,27 @@ res.sendFile(absolutePath);
 });
 // This creates a new listener at the /json endpoint
 app.get("/json", (req, res) => {
+let message = "Hello json";
+  
+  // 2. Check the "switch" inside the handler
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
 
   res.json({
-    "message": "Hello json"
+    "message": message
+  });
+});
+
+app.get("/now", (req, res, next) => {
+  // 1. Logic: Generate the time and attach it to the request object
+  req.time = new Date().toString();
+  next(); 
+}, (req, res) => {
+  // 2. Output: Send the object to the browser
+  // Optional: console.log(req.time); // This would make it show in the terminal!
+  res.json({
+    time: req.time
   });
 });
 

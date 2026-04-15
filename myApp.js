787cea6c4540b56010f0e5dba2f86edf354a4f4b
marcotramacere1982-lgtm <marcotramacere1982@gmail.com>
2026-04-15 16:08:23 +0000

    final

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
-
+let cors = require('cors');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
 
   const method = req.method;
@@ -13,7 +15,7 @@ app.use(function(req, res, next) {
 });
 
 app.use("/public", express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", function(req, res) {
 //  res.send("Hello Express");
 const absolutePath = __dirname + "/views/index.html";
@@ -51,49 +53,26 @@ app.get("/:word/echo", (req, res) => {
   });
 });
 
-app.get("/name", (req, res) => {
-  res.json({ 
-    name: req.query.first + " " + req.query.last 
+app.get("/name", function(req, res) {
+  // GET uses req.query
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  res.json({
+    name: firstName + " " + lastName
   });
 });
 
-app.post("/name", (req, res) => {
-  res.json({ 
-    name: req.body.first + " " + req.body.last 
-  });
+app.post("/name", function(req, res) {
+  // 1. Explicitly pull the data
+  var first = req.body.first;
+  var last = req.body.last;
+  
+  // 2. Build the object exactly as requested
+  var result = { 
+    name: first + " " + last 
+  };
+  
+  // 3. Send it
+  res.json(result);
 });
-
-
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
